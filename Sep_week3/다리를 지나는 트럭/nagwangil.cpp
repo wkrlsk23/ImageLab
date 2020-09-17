d384a6f95787336c7521c11dd0500b373ad68f66
nagwangil <63042908+nagwangil@users.noreply.github.com>
2020-09-17 16:42:54 +0900

    Update nagwangil.cpp

--- Sep_week3/다리를 지나는 트럭/nagwangil.cpp
+++ Sep_week3/다리를 지나는 트럭/nagwangil.cpp
@@ -6,24 +6,24 @@ using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
 
-    int answer = 0;
-    bool first = true;     
+    int answer = 0; //답 ==시간초
+    bool first = true; //첫 트럭이 올라가는지     
     vector<int> passing;//다리위 트럭들
     vector<int> passing_time; //다리 위에 있든 트럭들이 얼마나 오래있었는지
     vector<int> passied; //다리를 지난 트럭들
     
     while (true)
     {
-        answer++;
+        answer++; //시간초++
 
-        if (passing_time.size())
+        if (passing_time.size())//다리 위에 트럭이 있다고 할때
         {
             
-            if (passing_time.at(0) > bridge_length)//트럭이 다 지나갔는지
+            if (passing_time.at(0) > bridge_length)// 맨 앞 트럭이 다리를 건넜는지 확인
             {
-                weight += passing.at(0);
-                passing.erase(passing.begin());
-                passing_time.erase(passing_time.begin());
+                weight += passing.at(0); //다리를 지났으니 그만큼 여유 무게 +
+                passing.erase(passing.begin());//다리위 트럭들에서 삭제
+                passing_time.erase(passing_time.begin()); // 다 지나간 트럭의 시간초 삭제
             }
         }
 
@@ -32,7 +32,7 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
         {
             if (weight - truck_weights.at(0) >= 0)//다음 트럭이 다리위로 올라갈수 있는지
             {
-                passing.push_back(truck_weights.at(0));
+                passing.push_back(truck_weights.at(0));//트럭추가
                 
                 if (first)
                 {
@@ -40,6 +40,7 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
                     first = false;
                 }
                 else passing_time.push_back(1);
+                
                 weight -= truck_weights.at(0);
                 truck_weights.erase(truck_weights.begin());
             }
@@ -50,10 +51,10 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
         {
             passing_time.at(i)++;
         }
-        if (passing.size() ==0)
+        if (passing.size() ==0)//트럭이 다 지나갔는지 확인
         {
             cout << answer << endl;*/
-            return answer-1;
+            return answer-1; //무조건 여기서 종료, 0초 관련해서도 while문이 돌아서 -1을 해줬습니다
         }
 
     }
