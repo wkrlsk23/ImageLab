d333107aa3fd3c89807e3456e951013c4ca49221
shb206 <shb206@gmail.com>
2020-09-17 10:10:18 +0900

    no message

---
+++ Sep_week3/기능개발/Hyeonbin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] solution(int[] progresses, int[] speeds)
+    {
+        List<int> list = new List<int>();
+        int index = 0;
+        int finish;
+        int i;
+
+        while (index < progresses.Length)
+        {
+            finish = 0;
+
+            for (i = 0; i < progresses.Length; i++)
+            {
+                progresses[i] += speeds[i];
+            }
+
+            for (i = index; i < progresses.Length; i++)
+            {
+                if (progresses[i] < 100)
+                    break;
+
+                if (progresses[i] >= 100)
+                {
+                    index++;
+                    finish++;
+                }
+            }
+
+            if (finish != 0)
+            {
+                list.Add(finish);
+            }
+        }
+
+        int[] answer = list.ToArray();
+        return answer;
+    }
+}
---
+++ Sep_week3/다리를 지나는 트럭/Hyeonbin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int bridge_length, int weight, int[] truck_weights)
+    {
+        int answer = 0;
+        int sum_weight = 0;
+        int next = 0;
+        int[] progress = new int[truck_weights.Length];
+        int[] index = new int[truck_weights.Length];
+
+        for (int i = 0; i < truck_weights.Length; i++)
+        {
+            index[i] = 2;
+            progress[i] = bridge_length + 1;
+        }
+
+        while (true)
+        {
+            for (int i = 0; i < progress.Length; i++)
+            {
+                if (index[i] == 1)
+                {
+                    progress[i]--;
+                }
+
+                if (progress[i] == 0)
+                {
+                    index[i] = 0;
+                    sum_weight -= truck_weights[i];
+                }
+            }
+
+            if ((next < truck_weights.Length) && ((sum_weight + truck_weights[next]) <= weight))
+            {
+                sum_weight += truck_weights[next];
+                index[next] = 1;
+                next++;
+            }
+
+            if (progress[progress.Length - 1] == 0)
+                break;
+
+            answer++;
+        }
+
+        return answer;
+    }
+}
